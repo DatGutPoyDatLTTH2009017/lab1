25e36c2d1df68dce3f7402615a3ce3377113fbee
Your Name <you@example.com>
2021-06-10 08:08:36 +0700

    lab2,3

---
+++ lab2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace lab2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Vui lòng nhập mã SIN");
+            ;
+            int n = int.Parse(Console.ReadLine());
+            int sun = 0;
+            if (n.ToString().Length != 9)
+            {
+                Console.WriteLine("Chương trình chỉ nhận 9 số");
+            }
+            else
+            {
+                while (n != 0)
+                {
+                    sun = sun + n % 10;
+                    n = n / 10;
+                }
+
+                int total = sun % 10;
+                if (total == 0)
+                {
+                    Console.WriteLine("This is valid SIN");
+                }
+            }
+        }
+    }
+}
