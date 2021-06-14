e6e4734e0ea37ac5cc667c9671ff5f07e81f8eff
Your Name <you@example.com>
2021-06-14 21:40:52 +0700

    hello c#

---
+++ lab4/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace lab4
+{
+    public class Student:Person
+    {
+        public int Program { get; set; }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Email: {Email}, PhoneNumber: {PhoneNumber}, Program: {Program}";
+        }
+    }
+}
